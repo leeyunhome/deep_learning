7d4e74327983740383b72354fa9ab23e50ffaa1f
leeyunhome <leeyunhome@daum.net>
2020-07-14 22:08:08 +0900

    modify basic.py(upto # 1.6.2 pyplot의 기능)

--- basic.py
+++ basic.py
@@ -95,13 +95,31 @@
 # print(X[X>15])
 
 # 1.6.1 단순한 그래프 그리기
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# # 데이터 준비
+# x = np.arange(0, 6, 0.1)  # 0에서 6까지 -.1 간격으로 생성
+# y = np.sin(x)
+
+# # 그래프 그리기
+# plt.plot(x, y)
+# plt.show()
+
+# 1.6.2 pyplot의 기능
 import numpy as np
 import matplotlib.pyplot as plt
 
 # 데이터 준비
-x = np.arange(0, 6, 0.1)  # 0에서 6까지 -.1 간격으로 생성
-y = np.sin(x)
+x = np.arange(0, 6, 0.1)  # 0에서 6까지 0.1간격으로 생성
+y1 = np.sin(x)
+y2 = np.cos(x)
 
 # 그래프 그리기
-plt.plot(x, y)
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos")  # cos 함수는 점선으로 그리기
+plt.xlabel("x")  # x축 이름
+plt.ylabel("y")  # y축 이름
+plt.title('sin & cos')  # 제목
+plt.legend()
 plt.show()
