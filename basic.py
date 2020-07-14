4b343ef9cc66e0416c34d3af7fe7b5fd01534c62
leeyunhome <leeyunhome@daum.net>
2020-07-14 21:00:51 +0900

    modify basic.py(upto 1.4.2 클래스)

--- basic.py
+++ basic.py
@@ -8,3 +8,20 @@ def hello(object):
 
 hello("neuezeal!")
 
+print('# 1.4.2 클래스')
+# 1.4.2 클래스
+
+class Man:
+    def __init__(self, name):
+        self.name = name
+        print("Initialized!")
+
+    def hello(self):
+        print("Hello " + self.name + "!")
+
+    def goodbye(self):
+        print("Good-bye " + self.name + "!")
+
+m = Man("Neuezeal")
+m.hello()
+m.goodbye()
