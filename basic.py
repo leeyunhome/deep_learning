aadbbc1bf589c6d6eda1b7b9d09cead81bdf7cba
leeyunhome <leeyunhome@daum.net>
2020-07-14 21:20:32 +0900

    modify basic.py(upto 1.5.3 넘파이의 산술연산)

--- basic.py
+++ basic.py
@@ -28,11 +28,24 @@
 # m.goodbye()
 
 # 1.5 넘파이
-print("# 1.5 넘파이")
+# print("# 1.5 넘파이")
+
+# import numpy as np
+
+# x = np.array([1.0, 2.0, 3.0])
+# print(x)
+# print(type(x))
+
+# 1.5.3 넘파이의 산술연산
+print(" ")
+print("# 1.5.3 넘파이의 산술연산")
 
 import numpy as np
 
 x = np.array([1.0, 2.0, 3.0])
-print(x)
-print(type(x))
+y = np.array([2.0, 4.0, 6.0])
+print(x + y)  # 원소별 덧셈
+print(x - y)  # 원소별 뺄셈
+print(x * y)  # 원소별 곱셈
+print(x / y)  # 원소별 나눗셈
 
