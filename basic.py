513fd3c979064f76ed1ec9e7da55b144f23c8d6f
leeyunhome <leeyunhome@daum.net>
2020-07-14 21:14:53 +0900

    modify basic.py(upto 1.5.2 넘파이)

--- basic.py
+++ basic.py
@@ -1,27 +1,38 @@
 # 1.3.8 for 문
-for index in[1, 2, 3]:
-    print(index)
+# for index in[1, 2, 3]:
+#     print(index)
 
 # 1.3.9 함수
-def hello(object):
-    print("Hello " + object + "!")
+# def hello(object):
+#     print("Hello " + object + "!")
 
-hello("neuezeal!")
+# hello("neuezeal!")
 
-print('# 1.4.2 클래스')
+# print('# 1.4.2 클래스')
+
 # 1.4.2 클래스
 
-class Man:
-    def __init__(self, name):
-        self.name = name
-        print("Initialized!")
+# class Man:
+#     def __init__(self, name):
+#         self.name = name
+#         print("Initialized!")
 
-    def hello(self):
-        print("Hello " + self.name + "!")
+#     def hello(self):
+#         print("Hello " + self.name + "!")
 
-    def goodbye(self):
-        print("Good-bye " + self.name + "!")
+#     def goodbye(self):
+#         print("Good-bye " + self.name + "!")
 
-m = Man("Neuezeal")
-m.hello()
-m.goodbye()
+# m = Man("Neuezeal")
+# m.hello()
+# m.goodbye()
+
+# 1.5 넘파이
+print("# 1.5 넘파이")
+
+import numpy as np
+
+x = np.array([1.0, 2.0, 3.0])
+print(x)
+print(type(x))
+
