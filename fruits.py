9ac58747774fff4e888f872c45667c372e5781ed
leeyunhome <leeyunhome@daum.net>
2020-07-14 20:21:05 +0900

    add fruits.py

---
+++ fruits.py
@@ -0,0 +1,12 @@
+fruits = ['apple', 'apple', 'apple', 'banana', 'banana', 'kiwi', 'kiwi', 'strawberry']
+
+g = {}
+
+for f in fruits:
+    if f in g:
+        g[f] = g[f] + 1
+    else:
+        g[f] = 1
+
+print(type(g))
+print(g)
